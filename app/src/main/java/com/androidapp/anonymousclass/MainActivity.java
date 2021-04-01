eb38c55d7c3c4d655886a2361b239db014cafc03
Y-B Park <201921055@sangmyung.kr>
2021-04-01 13:53:37 +0900

    Anonymous inner class

---
+++ app/src/main/java/com/androidapp/anonymousclass/MainActivity.java
@@ -0,0 +1,31 @@
+package com.androidapp.anonymousclass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Button mButton1;
+    TextView mTextView1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mButton1 = findViewById(R.id.button1);
+        mTextView1 = findViewById(R.id.textView1);
+
+        mButton1.setOnClickListener(new MyOnClickListener());
+    }
+
+    private class MyOnClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            mTextView1.setText("You clicked button 1");
+        }
+    }
+}
